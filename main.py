de2bf6952abff3276a77c1c64a3f9f7629668b97
fungus <fungussh@gmail.com>
2026-02-14 12:10:05 +0000

    Adding end_time for when recording should stop

--- main.py
+++ main.py
@@ -15,8 +15,13 @@ def select_device():
 
 
 def run():
-
+    datetime_format = "%Y-%m-%d %H:%M:%S"
     time_interval = float(input("What length time intervals would you like(min): "))
+    end_time_string = input(
+        "What time would you like to stop recording(%Y-%m-%d %H:%M:%S): "
+    )
+    end_time = datetime.strptime(end_time_string, datetime_format)
+
     # time_interval = int(input("What length time intervals would you like(min): "))
     device_index = select_device()
 
@@ -24,8 +29,7 @@ def run():
     frame_length = 512 / 16000
     frame_number = math.ceil((time_interval * 60) / frame_length)
 
-    ending = False
-    while not ending:
+    while datetime.now() < end_time:
         audio = []
         start_time = datetime.now().strftime("%Y%m%d-%H:%M:%S")
         file_name = f"{start_time}_length{time_interval}.wav"
